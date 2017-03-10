c52ef7723033e3d63515cd51e2da73c89a9a18fe
Paul Tarr <tarrbot88@gmail.com>
2017-03-10 14:43:49 -0800

    Virtual and Overide

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,22 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp,Warning, TEXT("PlayerController does not possess a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Possessing: %s"),*(ControlledTank->GetName()));
+	}
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 
