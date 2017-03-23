d334baa96bc0408144992de95a363be59f375406
Paul Tarr <tarrbot88@gmail.com>
2017-03-23 13:22:37 -0700

    EditAnywhere vs EditDefaultsOnly UPROPERTY stuff

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -46,18 +46,20 @@ private:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+	
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+		TSubclassOf<AProjectile> ProjectileBluePrint;
+	
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
+		float ReloadTimeInSeconds = 3;
 
-	UPROPERTY(EditAnywhere, Category = Firing)
+	UPROPERTY(EditDefaultsOnly, Category = Firing)
 		float LaunchSpeed = 4000;
 
-	UPROPERTY(EditAnywhere, Category = Setup)
-		TSubclassOf<AProjectile> ProjectileBluePrint;
+	
 
 	// Local reference to barrel
 	UTankBarrel* Barrel = nullptr;
-
-	float ReloadTimeInSeconds = 3;
-
 	double LastFireTime = 0;
 
 };
